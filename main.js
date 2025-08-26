ad9b537c7b54005688e80ca2647aa1268920e4b6
Eddy Kuo <itsuki520@gmail.com>
2025-08-26 18:13:58 +0800

    Chore: Disable automatic DevTools opening

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function createWindow() {
   mainWindow.loadFile('index.html');
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
